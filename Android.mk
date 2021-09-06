67eb1b934ea45e39589671bc38ed559f15c55d5c
okay <okay@arkose>
2021-09-06 12:59:56 -0700

    [release] update to work with 1.0.11 GT and monkecomputers

--- Android.mk
+++ Android.mk
@@ -18,23 +18,23 @@ TARGET_ARCH_ABI := $(APP_ABI)
 
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
-# Creating prebuilt for dependency: beatsaber-hook - version: 2.0.3
+# Creating prebuilt for dependency: beatsaber-hook - version: 2.3.0
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_2_0_3
+LOCAL_MODULE := beatsaber-hook_2_3_0
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_0_3.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_3_0.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: monkecomputer - version: 1.6.0
+# Creating prebuilt for dependency: monkecomputer - version: 1.6.1
 include $(CLEAR_VARS)
 LOCAL_MODULE := monkecomputer
 LOCAL_EXPORT_C_INCLUDES := extern/monkecomputer
 LOCAL_SRC_FILES := extern/libmonkecomputer.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: quest-cosmetic-loader - version: 0.2.1
+# Creating prebuilt for dependency: quest-cosmetic-loader - version: 0.2.2
 include $(CLEAR_VARS)
-LOCAL_MODULE := quest-cosmetic-loader_0_2_1
+LOCAL_MODULE := quest-cosmetic-loader_0_2_2
 LOCAL_EXPORT_C_INCLUDES := extern/quest-cosmetic-loader
-LOCAL_SRC_FILES := extern/libquest-cosmetic-loader_0_2_1.so
+LOCAL_SRC_FILES := extern/libquest-cosmetic-loader_0_2_2.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: zip - version: 1.7.4
 include $(CLEAR_VARS)
@@ -48,26 +48,26 @@ LOCAL_MODULE := monkecodegen_0_6_0
 LOCAL_EXPORT_C_INCLUDES := extern/monkecodegen
 LOCAL_SRC_FILES := extern/libmonkecodegen.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: custom-types - version: 0.12.4
+# Creating prebuilt for dependency: custom-types - version: 0.12.7
 include $(CLEAR_VARS)
 LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
 LOCAL_SRC_FILES := extern/libcustom-types.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: gorilla-utils - version: 0.2.2
+# Creating prebuilt for dependency: gorilla-utils - version: 0.2.3
 include $(CLEAR_VARS)
 LOCAL_MODULE := gorilla-utils
 LOCAL_EXPORT_C_INCLUDES := extern/gorilla-utils
 LOCAL_SRC_FILES := extern/libgorilla-utils.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: modloader - version: 1.1.0
+# Creating prebuilt for dependency: modloader - version: 1.2.3
 include $(CLEAR_VARS)
 LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
 LOCAL_CPP_FEATURES += rtti exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: monkecodegen - version: 0.7.4
+# Creating prebuilt for dependency: monkecodegen - version: 0.8.0
 include $(CLEAR_VARS)
 LOCAL_MODULE := monkecodegen
 LOCAL_EXPORT_C_INCLUDES := extern/monkecodegen
@@ -82,10 +82,10 @@ LOCAL_SRC_FILES += $(call rwildcard,src/**,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_0_3
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_3_0
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_SHARED_LIBRARIES += monkecomputer
-LOCAL_SHARED_LIBRARIES += quest-cosmetic-loader_0_2_1
+LOCAL_SHARED_LIBRARIES += quest-cosmetic-loader_0_2_2
 LOCAL_SHARED_LIBRARIES += zip
 LOCAL_SHARED_LIBRARIES += gorilla-utils
 LOCAL_SHARED_LIBRARIES += monkecodegen
--- buildQmod.sh
+++ buildQmod.sh
@@ -1,2 +1,2 @@
-zip -j MonkeTrail.zip ./libs/arm64-v8a/libcustom-types.so ./libs/arm64-v8a/libbeatsaber-hook_2_0_3.so ./libs/arm64-v8a/libtrail.so ./cover.png ./mod.json 
+zip -j MonkeTrail.zip ./libs/arm64-v8a/libcustom-types.so ./libs/arm64-v8a/libbeatsaber-hook_2_3_0.so ./libs/arm64-v8a/libtrail.so ./libs/arm64-v8a/libzip.so ./cover.png ./mod.json 
 cp MonkeTrail.zip MonkeTrail.qmod
